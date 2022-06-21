bdedb72e06dffc6c9ca6e738c8a3dfb40d608dca
Louiks <kokot.jakub@wp.pl>
2022-06-21 14:04:16 +0200

    fixed new hall bug

--- Frontend/src/app/admin/newHall/new-hall.service.ts
+++ Frontend/src/app/admin/newHall/new-hall.service.ts
@@ -10,17 +10,23 @@ export class NewHallService {
 
   constructor(private http: HttpClient) { }
 
-  addNewHall(data: any) {
-    const seats = [];
+  addNewHall(data: boolean[][]) {
+    const seats = Array<Seat>();
     for(let i = 0; i < data.length; i++) {
       for(let j = 0; j < data[i].length; j++) {
-        seats.push({
-          row: i,
-          column: j,
-          isReseverd: !data[i][j],
-        });
+        const newSeat = new Seat();
+        newSeat.row = i.toString();
+        newSeat.column = j.toString();
+        newSeat.isReserved = !data[i][j];
+        seats.push(newSeat);
       }
    }
-    return this.http.post(`${baseUrl}/seats`, seats, { responseType: 'text' });
+    return this.http.post(`${baseUrl}/seats`, seats, { responseType: 'text', headers: {'Content-type': 'application/json'}});
   }
+}
+
+export class Seat {
+  row?: string;
+  column?: string;
+  isReserved?: boolean
 }
